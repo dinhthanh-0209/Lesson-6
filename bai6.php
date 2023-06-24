10d79a7b47e028ad7cd58baa8fce22d8a8461eb5
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-06-24 09:19:41 +0700

    Add files via upload

---
+++ bai6.php
@@ -0,0 +1,558 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="styte.css" />
+  </head>
+  <body>
+    <!-- header -->
+    <header id="header">
+      <div class="header-top">
+        <!-- top head -->
+        <div class="logo">
+          <p>Biccas</p>
+        </div>
+        <div class="navigation-bar">
+          <!--  menu -->
+          <nav class="main-menu">
+            <ul>
+              <li class="main-menu-item main-menu-item-active"><a href="">Home</a></li>
+              <li class="main-menu-item">
+                <a href="">Product</a>
+              </li>
+              <li class="main-menu-item"><a href="">FAQ</a></li>
+              <li class="main-menu-item"><a href="">Blog</a></li>
+              <li class="main-menu-item"><a href="">About us</a></li>
+            </ul>
+          </nav>
+          <!-- button -->
+          <div class="header-button-group">
+            <a href="" class="none-text_decoration bar__item">Login</a>
+            <a href="" class="none-text_decoration bar__item">Sign up</a>
+          </div>
+        </div>
+      </div>
+      <!-- bot head -->
+      <div class="header-bottom">
+        <!-- left -->
+        <div class="banner-description">
+          <p class="banner-description-title">
+          We're here to Increase your Productivity          
+          </p>
+          <div class="banner-description-break">
+            <img src="Untitled-4.png" alt="">
+          </div>
+          <p class="banner-description-content">
+            Let's make your work more organize and easlily using the Taskio Dashboard with many of the latest features in managing work every day.
+          </p>
+          <div class="banner-description-btn">
+            <button class="btn__free-trial">
+              Try free trial
+            </button>
+            <button class="btn__view-demo">
+              <i class="fa-regular fa-circle-play"></i>
+              <span>View demo</span>
+            </button>
+          </div>
+        </div>
+        <!-- right -->
+        <div class="banner-image-wrap">
+          <img src="person.png" alt="" />
+          <img src="card.png" alt="" />
+          <form action="">
+            <div class="form-group">
+              <p class="text-grey">Enter amount</p>
+              <input class="form-control-input" type="text" value="$450..00" />
+              <button class="btn btn-info btn-round btn-sm">Send</button>
+            </div>
+          </form>
+          <i class="fa-solid fa-square-check"></i>
+          <div class="total-income">
+            <div>
+              <p class="text-grey">Total Income</p>
+              <p>$250.00</p>
+            </div>
+            <i class="fa-sharp fa-solid fa-chart-simple"></i>
+          </div>
+        </div>
+      </div>
+      <div class="bottom__content">
+        <div class="bottom__content-text">
+          More than 25,000 teams use Collabs
+        </div>
+        <div class="bottom__content-list">
+          <span class="bottom__content-list--item">
+            <i class="fa-brands fa-unsplash"></i>Unsplash
+          </span>
+          <span class="bottom__content-list--item">
+            <i class="fa-regular fa-square"></i>Notion
+          </span>
+          <span class="bottom__content-list--item">
+            <i class="fa-brands fa-intercom"></i>INTERCOM
+          </span>
+          <span class="bottom__content-list--item">
+            <i class="fa-solid fa-bars-staggered"></i>descript
+          </span>
+          <span class="bottom__content-list--item">
+            <i class="fa-solid fa-rotate-left fa-rotate-180"></i>grammarly
+          </span>    
+        </div>
+      </div>
+    </header>
+    <!-- main -->
+    <main>
+    </main>
+    <!-- ảnh 4-->
+<div class="main2">
+  <div class="how">
+    <h3>How we support our pratner all over the world</h3>
+    <p id="how-text2">SaaS become a commom delivery model for many business application, including ofice software, mesaging software, payroll processing software, DBMS software mangament software</p>
+    <div class="rating">
+      <ul>
+        <li> 
+          <img src="star.png" alt="">
+        </li>
+        <li>
+          <img src="star.png" alt="">
+        </li>
+        <li>
+          <img src="star.png" alt="">
+        </li>
+        <li>
+          <img src="star.png" alt="">
+        </li>
+        <li>
+          <img src="star.png" alt="">
+        </li>
+      </ul>
+      <p>4.9/5 rating</p>
+      <p> databricks</p>
+    </div>
+    <div class="rating">
+      <ul>
+        <li> 
+          <img src="star.png" alt="">
+        </li>
+        <li>
+          <img src="star.png" alt="">
+        </li>
+        <li> 
+          <img src="star.png" alt="">
+        </li>
+        <li>
+          <img src="star.png" alt="">
+        </li>
+      </ul>
+      <p>4.8/5 rating</p>
+      <p> databricks</p>
+    </div>
+  </div>
+  <div class="how" >
+      <div class="pub">
+        <ul>
+          <li>
+            <img src="analyst.png" alt="">
+          </li>
+          <li>
+            <p>Publishing</p>
+          </li>
+        </ul>
+        <p id="pub-text">Plan, collaborate, and, publishing your 
+          content that drivees meaning engagement and growth for your banrd</p>
+      </div>
+  <!---->
+  <div class="ana">
+    <ul>
+      <li>
+        <img src="analyst.png" alt="">
+      </li>
+      <li>
+        <p>Analytics</p>
+      </li>
+    </ul>
+    <p id="ana-text">Analyze your performance and create goegeous report</p>
+  </div>
+<!---->
+<div class="eng">
+  <ul>
+    <li>
+      <img src="analyst.png" alt="">
+    </li>
+    <li>
+      <p>Engagement</p>
+    </li>
+  </ul>
+  <p id="eng-text">Quiuckly navigate you anda engage with your adience</p>
+</div>
+<!---->
+</div>
+</div>
+<!-- Our Features you cab get-->
+<div class="main3">
+  <ul>
+    <li><h3> Our Features you cab get</h3>  
+    </li>
+    <li>
+      <p id="main3-text2">We offer a variety of interesting features that you can help increase your 
+        productivity at work and manage your projiect easaly</p>
+    </li>
+    <li>
+      <button>Get Started</button>
+    </li>
+  </ul>
+</div>
+<!-- ảnh sau Our Features-->
+<div class="main4">
+<div class="main4-img">
+  <ul>
+    <li> 
+      <img src="Untitled-4.png" alt="">
+    </li>
+    <li> 
+      <h3>Collboration Teams</h3>
+    </li>
+    <li>
+      <p>Here you can handle projects together with team virtually</p>
+    </li>
+   
+    
+  </ul>
+</div>
+<div class="main4-img">
+  <ul>
+    <li>
+      <img src="Untitled-4.png" alt="">
+    </li>
+    <li>
+      <h3>Clould Storage</h3>
+    </li>
+    <li>
+      <p>No need to worry about storage because we provide storage up to 2TB</p>
+    </li>
+   
+  </ul>
+</div>
+<div class="main4-img">
+  <ul>
+    <li>
+      <img src="Untitled-4.png" alt="">
+    </li>
+    <li>
+      <h3> Daily Analytics</h3>
+    </li>
+    <li>
+      <p>We always provide useful information to make it easier for you every day</p>
+    </li>
+  </ul>
+</div>
+</div>
+<!-- main5: ảnh 5-->
+<div>
+  <div class="main5">
+    <div><h3>What Benifit Will You Get</h3></div>
+<!--1-->
+    <div class="main5-text">
+      <ul>
+        <li>
+          <img src="analyst.png" alt="">
+        </li>
+        <li><p>Free Consulting With Exporot Saving Money</p></li>
+      </ul>
+    </div>
+<!--2-->
+    <div class="main5-text">
+      <ul>
+        <li>
+          <img src="analyst.png" alt="">
+        </li>
+        <li><p>Online Banking</p></li>
+      </ul>
+    </div>
+<!--3-->
+    <div class="main5-text">
+      <ul>
+        <li>
+          <img src="analyst.png" alt="">
+        </li>
+        <li><p>Invesment Report Every Month</p></li>
+      </ul>
+    </div>
+<!--4-->
+<div class="main5-text">
+  <ul>
+    <li>
+      <img src="analyst.png" alt="">
+    </li>
+    <li><p>Saving Money For The Future</p></li>
+  </ul>
+</div>
+<!--5-->
+<div class="main5-text">
+  <ul>
+    <li>
+      <img src="analyst.png" alt="">
+    </li>
+    <li><p>Online Transection</p></li>
+  </ul>
+</div>
+<!--hết-->
+  </div>
+  <div class="main5">
+    <div class="main5-img">
+      <img src="mac.png" alt=""></div>
+  </div>
+</div>
+<!---->
+<div class="choose"> <h3 id="h31">Choose Plan </h3>
+  <h3 id="h32">That's Right For You</h3>
+<p>Choose plan that works best for you, feel free to contact us</p></div>
+<div class="choose-btn">
+  <ul><li><button>Bil Monthly</button></li>
+  <li><button id="bil-yearly">Bil Yearly</button></li></ul>
+</div>
+<!--Form-->
+<div class="form">
+<div> <h4>Free</h4>
+      <p>Have a go and tesst your superpowers</p>
+      <h4>$0</h4>
+</div>
+<div class="form1">
+  <ul>
+    <li>
+      <img src="star.png" alt="">
+    </li>
+    <li><p>2 User</p></li>
+  </ul>
+</div>
+<div class="form1">
+  <ul>
+    <li>
+      <img src="star.png" alt="">
+    </li>
+    <li><p>2 Files</p></li>
+  </ul>
+</div>
+<div class="form1">
+  <ul>
+    <li>
+      <img src="star.png" alt="">
+    </li>
+    <li><p>Public Share & Comment</p></li>
+  </ul>
+</div>
+<div class="form1">
+  <ul>
+    <li>
+      <img src="star.png" alt="">
+    </li>
+    <li><p>Chat Support</p></li>
+  </ul>
+</div>
+<div class="form1">
+  <ul>
+    <li>
+      <img src="star.png" alt="">
+    </li>
+    <li><p>New income apps</p></li>
+  </ul>
+</div>
+<div>
+  <button>Signup for free</button>
+</div>
+</div>
+<!--form 2-->
+<div class="form">
+  <div class="form2">
+  
+  <div> <h4>Pro</h4>
+    <p>Experiment the power of infinite possibition</p>
+    <h4>$8</h4>
+    <button id="btn8">Save $50 a year</button>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>4 User</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>All apps</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>Inlimited editable exports</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>Folders and collaboration</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>All incoming apps</p></li>
+</ul>
+</div>
+<div>
+<button id="go-to-pro">Go to pro</button>
+</div>
+</div>
+</div>
+<!--form 3-->
+<div class="form">
+  <div> <h4>Business</h4>
+    <p>Unveil new superpowers and join the Design Leaque</p>
+    <h4>$16</h4>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>All the future of pro plan</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>Account success Manager</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>Single Sign-On (SSO)</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>Co-conception program</p></li>
+</ul>
+</div>
+<div class="form1">
+<ul>
+  <li>
+    <img src="star.png" alt="">
+  </li>
+  <li><p>Collaboration-Soon</p></li>
+</ul>
+</div>
+<div>
+<button>Go to Business</button>
+</div>
+</div>
+<!--People are Saying About DoWith-->
+<br>
+<div class="people">
+<h3>People are Saying </h3>
+<h3>About DoWith</h3>
+<p>Everthing you need to accept to payent and grow <br>
+your money of manage anywhere on planet</p>
+<h1>//</h1>
+<p>I am very helped bu this E-wallet application, my<br>
+days are vary easy to use this application and its very<br>
+helpful in my life, even I can pay a short time</p>
+<p>_ Aria Zinnanrio</p>
+<div class="people-img">
+  <ul><li>
+    <img src="person.png" alt="">
+  </li>
+  <li>
+    <img src="person.png" alt="">
+  </li>
+  <li>
+    <img src="person.png" alt="">
+  </li>
+  <li>
+    <img src="Untitled-4.png" alt="">
+  </li>
+</ul>
+</div>
+</div>
+<!---->
+<div class="people">
+  <form action="get-started" method="post">
+    <h3>Get-Started<br>  </h3>
+    <label for="">Email</label><br>
+    <input type="text" name="email" value="Enter your email"><br>
+    <label for="">Message<br>
+ <input type="text" name="message" id="" value="What are you say?">
+    </label><br>
+   <button id="people-btn">Request Demo</button>
+    <label for=""></label>
+    <p id="or"> <a href="">or Start Free Trial</a></p>
+  </form>
+</div>
+    <!-- footer -->
+    <footer id="footer">
+      <div class="footer0">
+          <div class="logo">
+            <p>Biccas</p>
+          </div>
+          <p style="font-size: 15px;">Get started noew try our product</p>
+          <div>
+            <input type="text" placeholder="Enter your email here" />
+            <img src="" alt="" />
+          </div>
+      </div>
+        <div class="footer">
+          <div class="footer1">
+          <ul>
+            <li><a href="">Support</a></li>
+            <li><a href="">Help centre</a></li>
+            <li><a href="">Account information</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact us</a></li>
+          </ul>
+          </div>
+          <div class="footer1">
+          <ul>
+            <li><a href="">Help And Solution</a></li>
+            <li><a href="">Talk to support</a></li>
+            <li><a href="">Support docs</a></li>
+            <li><a href="">System status</a></li>
+            <li><a href="">Covid responde</a></li>
+          </div>
+          <div class="footer1">
+          <ul>
+            <li><a href="">Product</a></li>
+            <li><a href="">Update</a></li>
+            <li><a href="">Security</a></li>
+            <li><a href="">Beta test</a></li>
+            <li><a href="">Pricing product</a></li>
+          </ul>
+          </div>
+      </div>
+    <div class="nav">
+       <p>@ 2022 Biccas Inc. Copyright and rights reserved</p>
+       <p>Terms and Conditions * Privacy polocy</p>
+    </div>
+    </footer>
+  </body>
+</html>
